dd7413f1b0debc7c5ce5f76f3706b132bede8893
Marco Sampellegrini <babbonatale@alpacaaa.net>
2017-12-05 22:12:33 +0000

    Day5 done.

--- src/Day5.purs
+++ src/Day5.purs
@@ -16,13 +16,15 @@ import Node.Encoding as Encoding
 newtype Instruction =
     Instruction Int
 
+newtype Step =
+    Step Int
 
 solution _ = do
     input <- FS.readTextFile Encoding.UTF8 "input/day5.txt"
 
     pure $
         String.split (String.Pattern "\n") input
-            # solve
+        # solve
 
 print input expected = do
     log $ "Solving " <> (show input) <> ": " <> (show $ solve input) <> " expecting: " <> (show expected)
@@ -32,19 +34,23 @@ solve :: Array String -> Int
 solve input =
     traverse Int.fromString input
     # maybe [] (map Instruction)
-    # solve' 0
+    # solve' (Step 0) 0
 
 
-solve' :: Int -> Array Instruction ->  Int
-solve' index stack =
+solve' :: Step -> Int -> Array Instruction ->  Int
+solve' (Step step) index stack =
     case Array.index stack index of
         Just (Instruction instruction) ->
-            stack
-            # Array.updateAt index (Instruction $ instruction + 1)
-            # maybe 0 (solve' $ index + instruction)
+            let
+                updatedStack =
+                    stack
+                    # Array.updateAt index (Instruction $ instruction + 1)
+                    # maybe [] id
+            in
+            solve' (Step $ step + 1) (index + instruction) updatedStack
 
         Nothing ->
-            index
+            step
 
 
 test = do
